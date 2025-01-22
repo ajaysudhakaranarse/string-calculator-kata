9a15eabb2fc7f495d13d2dc01a6201c96a236bd0
ajay anarse <ajayanarse@ajays-MacBook-Pro.local>
2025-01-22 18:42:04 +0530

    Done changes
    1. Fixed rubocop offenses
    2. Ignore number greater that 1000 in string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
 class StringCalculator
-    attr_accessor :delimiter
+  attr_accessor :delimiter
 
-    def initialize(delimiter = ',')
-        delimiter = delimiter
-    end
+  def initialize(delimiter = ',')
+    delimiter
+  end
 
-    def add(numbers)
-        numbers = parse_numbers(numbers)
-        numbers.sum
-    end
+  def add(numbers)
+    numbers = parse_numbers(numbers)
+    numbers.reject { |no| no > 1000 }.sum
+  end
 
-    def parse_numbers(numbers)
-        numbers = numbers.split(delimiter)
-        numbers.map(&:to_i)
-    end
+  def parse_numbers(numbers)
+    numbers = numbers.split(delimiter)
+    numbers.map(&:to_i)
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,12 +4,22 @@ require 'rspec'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
-    describe '#add' do
-        let(:string_calculator_obj) { StringCalculator.new }
+  describe '#add' do
+    let(:string_calculator_obj) { StringCalculator.new }
 
-        it 'return sum of numbers' do
-            result = string_calculator_obj.add('1, 5')
-            expect(result).to eq(6)
-        end
+    it 'return sum of numbers' do
+      result = string_calculator_obj.add('1, 5')
+      expect(result).to eq(6)
     end
+
+    it 'return 0 for empty string' do
+      result = string_calculator_obj.add('')
+      expect(result).to eq(0)
+    end
+
+    it 'ignores number greater that 1000' do
+      result = string_calculator_obj.add('1, 2, 1001, 1002, 3')
+      expect(result).to eq(6)
+    end
+  end
 end
