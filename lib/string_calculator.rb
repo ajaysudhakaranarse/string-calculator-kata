93bdb146dda2b3dd677c0e7ac6483819dfcd4965
ajay anarse <ajayanarse@ajays-MacBook-Pro.local>
2025-01-22 11:08:16 +0530

    Adding #Add method with basic functionality

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class StringCalculator
+    attr_accessor :delimiter
+
+    def initialize(delimiter = ',')
+        delimiter = delimiter
+    end
+
+    def add(numbers)
+        numbers = parse_numbers(numbers)
+        numbers.sum
+    end
+
+    def parse_numbers(numbers)
+        numbers = numbers.split(delimiter)
+        numbers.map(&:to_i)
+    end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,12 @@ require 'rspec'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+    describe '#add' do
+        let(:string_calculator_obj) { StringCalculator.new }
+
+        it 'return sum of numbers' do
+            result = string_calculator_obj.add('1, 5')
+            expect(result).to eq(6)
+        end
+    end
 end
